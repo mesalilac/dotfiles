49565eab35e8d5d08066aaffa746f59b2d7bb006
Senpai-10 <bmjfdrh@gmail.com>
2021-02-11 05:31:02 +0300

    add vim, g++, steam

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 
 # chmod +x <fileName>
 
-echo "Installing some basic system utilities."
+echo -e "\e[92m\e[1mInstalling\e[0m some \e[1mbasic system utilities\e[0m."
 
 cd ~
 sudo apt-get update -qq
@@ -15,17 +15,17 @@ sudo apt install -yy ubuntu-restricted-extras
 
 sudo apt-get install -yy htop
 
-echo "Installing Google Chrome."
+echo -e "\e[92m\e[1mInstalling\e[0m \e[1mGoogle Chrome\e[0m."
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install -yy ./google-chrome-stable_current_amd64.deb
 
-echo "Installing SimpleScreenRecorder and Spotify."
+echo -e "\e[92m\e[1mInstalling\e[0m \e[1mSimpleScreenRecorder\e[0m and \e[1mSpotify\e[0m."
 
 sudo apt-get update -qq
 sudo apt-get install -yy simplescreenrecorder spotify-client
 
-echo "Installing discord"
+echo -e "\e[92m\e[1mInstalling\e[0m \e[1mdiscord\e[0m"
 
 sudo apt update
 sudo apt install -yy gdebi-core wget
@@ -33,26 +33,36 @@ wget -O ~/discord.deb "https://discordapp.com/api/download?platform=linux&format
 sudo gdebi -n ~/discord.deb
 
 
-echo "Installing development stuff."
+echo -e "\e[92m\e[1mInstalling\e[0m \e[1mdevelopment\e[0m stuff."
 
-# install vscode
+
+echo -e "\e[92m\e[1mInstalling\e[0m \e[1mvscode\e[0m."
 sudo apt install -yy software-properties-common apt-transport-https wget
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 sudo apt install -yy code
 
-# install node, npm
 
+echo -e "\e[92m\e[1mInstalling\e[0m \e[1mnode, npm\e[0m."
 sudo apt install -yy nodejs
 sudo apt install -yy npm
 
-# install python3
 
+echo -e "\e[92m\e[1mInstalling\e[0m \e[1mpython3\e[0m."
 sudo apt-get install -yy python3
 
-# install git
 
+echo -e "\e[92m\e[1mInstalling\e[0m \e[1mgit\e[0m."
 sudo apt install -yy git
 
+echo -e "\e[92m\e[1mInstalling\e[0m \e[1mvim\e[0m."
+sudo apt install -yy vim
 
 
+echo -e "\e[92m\e[1mInstalling\e[0m \e[1mg++\e[0m."
+sudo apt install -yy g++
+
+
+# gaming
+echo -e "\e[92m\e[1mInstalling\e[0m \e[1msteam\e[0m."
+sudo apt install -yy steam
