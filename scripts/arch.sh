98637c2900c9db70b1ce46148f46e87aa97cc5d9
senpai-10 <bmjfdrh@gmail.com>
2021-03-10 00:24:56 +0300

    rewrite for smenu

--- scripts/arch.sh
+++ scripts/arch.sh
@@ -9,13 +9,129 @@ configure () {
 }
 
 
-PS3='Please enter your choice: '
-options=("install dwm" "install all packages" "install kitty" "install st" "Quit")
+if pacman -Qe "yay" > /dev/null ; then
+    echo
+else
+    cd ~
+    git clone https://aur.archlinux.org/yay.git
+    cd yay
+    makepkg -si
+fi
 
-select opt in "${options[@]}"
-do
-    case $opt in
-        "install dwm")
+if pacman -Qe "smenu" > /dev/null ; then
+    echo
+else
+    yay -S smenu
+fi
+
+clear
+
+main () {
+
+    option_1="Packages"
+    option_2="Window manager"
+    option_3="Terminal"
+    option_4="Quit"
+
+    options="
+    $option_1\n$option_2\n$option_3\n$option_4
+    "
+
+    menu=$(echo $options | smenu -c -W $'\n' -N -M -m "install")
+
+    if [[ $menu == $option_1 ]]
+    then
+        clear
+        cd ~
+        installing "zip"
+        yes | sudo pacman -S zip
+        installing "unzip"
+        yes | sudo pacman -S unzip
+        installing "neofetch"
+        yes | sudo pacman -S neofetch
+        installing "neovim"
+        yes | sudo pacman -S neovim
+        installing "alsa-utils"
+        yes | sudo pacman -S alsa-utils
+        installing "vscode"
+        yes | sudo pacman -S code
+        installing "discord"
+        yes | sudo pacman -S discord
+        installing "feh"
+        yes | sudo pacman -S feh
+        installing "git"
+        yes | sudo pacman -S git
+        
+        menu=$(echo "GoogleChrome\nFirefox" | smenu -c -W $'\n' -N -M -m "browser")
+
+        if [[ $menu == "GoogleChrome" ]]
+        then
+            yay -S google-chrome
+        fi
+
+        if [[ $menu == "Firefox" ]]
+        then
+            yes | sudo pacman -S firefox
+        fi
+
+        installing "htop"
+        yes | sudo pacman -S htop
+        installing "imagemagick"
+        yes | sudo pacman -S imagemagick
+        installing "nodejs"
+        yes | sudo pacman -S nodejs
+        installing "noto fonts emoji"
+        yes | sudo pacman -S noto-fonts-emoji
+        yay -S noto-fonts-sc
+
+        installing "npm"
+        yes | sudo pacman -S npm
+        installing "pulseaudio"
+        yes | sudo pacman -S pulseaudio
+        installing "python3"
+        yes | sudo pacman -S python3
+        installing "python pip"
+        yes | sudo pacman -S python-pip
+        installing "rxvt unicode"
+        yes | sudo pacman -S rxvt-unicode
+        installing "scrot"
+        yes | sudo pacman -S scrot
+        installing "ttf font awesome"
+        yes | sudo pacman -S ttf-font-awesome
+        installing "which"
+        yes | sudo pacman -S which
+        installing "wget"
+        yes | sudo pacman -S wget
+        installing "curl"
+        yes | sudo pacman -S curl
+        installing "xbindkeys"
+        yes | sudo pacman -S xbindkeys
+        configure "xbindkeys"
+        echo '"setxkbmap us"' >> ~/.xbindkeysrc
+        echo -e "\tAlt+Shift+1" >> ~/.xbindkeysrc
+        echo '"setxkbmap ara"' >> ~/.xbindkeysrc
+        echo -e "\tAlt+Shift+2" >> ~/.xbindkeysrc
+
+
+        menu=$(echo "back" | smenu -c -W $'\n' -N -M -m "Installation finished")
+
+        if [[ $menu == "back" ]]
+        then
+            clear
+            main
+        fi
+
+    fi
+
+    if [[ $menu == $option_2 ]]
+    then
+        clear
+        
+        menu=$(echo "dwm\nback" | smenu -c -W $'\n' -N -M -m "$option_2")
+
+        if [[ $menu == "dwm" ]]
+        then
+            clear
             installing "dwm"
             mkdir -p ~/wm
             cd ~/wm &&
@@ -23,106 +139,94 @@ do
             cd dwm
             sudo make install &&
             cd ..
+
             installing "dmenu"
             git clone https://github.com/Senpai-10/dmenu.git &&
             cd dmenu
             sudo make install &&
             cd ..
+            
             installing "slstatus"
             git clone https://github.com/Senpai-10/slstatus.git &&
             cd slstatus
             sudo make install &&
             cd ..
-            ;;
 
-        "install all packages")
-            cd ~
-            installing "zip"
-            yes | sudo pacman -S zip
-            installing "unzip"
-            yes | sudo pacman -S unzip
-            installing "neofetch"
-            yes | sudo pacman -S neofetch
-            installing "neovim"
-            yes | sudo pacman -S neovim
-            installing "alsa-utils"
-            yes | sudo pacman -S alsa-utils
-            installing "vscode"
-            yes | sudo pacman -S code
-            installing "discord"
-            yes | sudo pacman -S discord
-            installing "feh"
-            yes | sudo pacman -S feh
-            installing "git"
-            yes | sudo pacman -S git
-            
-            installing "google chrome"
-            cd Downloads
-            git clone https://aur.archlinux.org/google-chrome.git
-            cd google-chrome/
-            yes | makepkg -si
-            cd ~
-            
-            installing "htop"
-            yes | sudo pacman -S htop
-            installing "imagemagick"
-            yes | sudo pacman -S imagemagick
-            installing "nodejs"
-            yes | sudo pacman -S nodejs
-            installing "noto fonts emoji"
-            yes | sudo pacman -S noto-fonts-emoji
+            clear
+            menu=$(echo "back" | smenu -c -W $'\n' -N -M -m "Installation finished")
 
-            installing "noto fonts sc"
-            cd Downloads
-            https://aur.archlinux.org/noto-fonts-sc.git
-            cd noto-fonts-sc
-            yes | makepkg -si
-            cd ~
+            if [[ $menu == "back" ]]
+            then
+                clear
+                main
+            fi
 
-            installing "npm"
-            yes | sudo pacman -S npm
-            installing "pulseaudio"
-            yes | sudo pacman -S pulseaudio
-            installing "python3"
-            yes | sudo pacman -S python3
-            installing "python pip"
-            yes | sudo pacman -S python-pip
-            installing "rxvt unicode"
-            yes | sudo pacman -S rxvt-unicode
-            installing "scrot"
-            yes | sudo pacman -S scrot
-            installing "ttf font awesome"
-            yes | sudo pacman -S ttf-font-awesome
-            installing "which"
-            yes | sudo pacman -S which
-            installing "wget"
-            yes | sudo pacman -S wget
-            installing "curl"
-            yes | sudo pacman -S curl
-            installing "xbindkeys"
-            yes | sudo pacman -S xbindkeys
-            configure "xbindkeys"
-            echo '"setxkbmap us"' >> ~/.xbindkeysrc
-            echo -e "\tAlt+Shift+1" >> ~/.xbindkeysrc
-            echo '"setxkbmap ara"' >> ~/.xbindkeysrc
-            echo -e "\tAlt+Shift+2" >> ~/.xbindkeysrc
-            ;;
-        "install kitty")
-            cd ~
+        fi
+
+        if [[ $menu == "back" ]]
+        then
+            clear
+            main
+        fi
+    fi
+
+    if [[ $menu == $option_3 ]]
+    then
+        clear
+
+        menu=$(echo "st\nkitty\nback" | smenu -c -W $'\n' -N -M -m $option_3)
+
+        if [[ $menu == "st" ]]
+        then
+            clear
+            menu=$(echo "Luke st\nsuckless st\nback" | smenu -c -W $'\n' -N -M -m "st build")
+
+            if [[ $menu == "Luke st" ]]
+            then
+                installing "Luke's fork of st"
+                cd ~
+                git clone https://github.com/LukeSmithxyz/st
+                cd st
+                sudo make install
+                cd ~
+
+                clear
+                menu=$(echo "back" | smenu -c -W $'\n' -N -M -m "Installation finished")
+
+                if [[ $menu == "back" ]]
+                then
+                    clear
+                    main
+                fi
+
+            fi
+
+            if [[ $menu == "suckless st" ]]
+            then
+                cd ~
+                git clone https://git.suckless.org/st
+                cd st
+                sudo make install
+                cd ~
+            fi
+
+            if [[ $menu == "back" ]]
+            then
+                clear
+                main
+            fi
+        fi
+
+        if [[ $menu == "kitty" ]]
+        then
             installing "kitty"
             yes | sudo pacman -S kitty && echo -e "\n"
             configure "kitty"; echo -e "\n"
+            clear
 
+            menu=$(echo "Theme (1)\ndefault" | smenu -c -W $'\n' -N -M -m "kitty Theme")
 
-            echo "1) Theme (1)"
-            echo -e "0) Default\n"
-
-            echo -e "Please enter your choice: "
-            read theme
-
-            echo ""
-
-            if [[ $theme == 1 ]]
+            if [[ $menu == "Theme (1)" ]]
             then
                 mkdir -p ~/.config/kitty/themes
                 curl https://raw.githubusercontent.com/Senpai-10/my-linux/main/themes/1/1.conf \
@@ -130,39 +234,37 @@ do
 
                 echo "include ~/.config/kitty/themes/1.conf" >> ~/.config/kitty/kitty.conf
             fi
-            
-            echo "sync_to_monitor (y/n): "
-            read stm
 
-            if [[ $stm == "y" ]]
+            clear
+            menu=$(echo "y\nn" | smenu -c -W $'\n' -N -M -m "sync_to_monitor")
+
+            if [[ $menu == "y" ]]
             then
                 echo "sync_to_monitor yes" >> ~/.config/kitty/kitty.conf
             fi
 
-            echo "background_opacity (1 is solid): "
-            read bo
-            echo "background_opacity $bo" >> ~/.config/kitty/kitty.conf
-            
-            ;;
-        "install st")
-            cd ~
-            echo "1) Luke's fork of st"
-            echo ": "
-            read st
+            clear
 
-            if [[ $st == 1 ]]
-            then
-                installing "Luke's fork of st"
-                git clone https://github.com/LukeSmithxyz/st
-                cd st
-                sudo make install
-                cd ~
-            fi
-            
-            ;;
-        "Quit")
-            break
-            ;;
-        *) echo "invalid option $REPLY";;
-    esac
-done
+            menu=$(echo "1\n.9\n.8\n.7\n.6\n.5\n.4\n.3\n.2\n.1" | smenu -c -W $'\n' -n10 -N -M -m "background_opacity")
+
+            echo "background_opacity $menu" >> ~/.config/kitty/kitty.conf
+        fi
+
+        if [[ $menu == "back" ]]
+        then
+            clear
+            main
+        fi
+
+    fi
+
+    if [[ $menu == $option_4 ]]
+    then
+        clear
+        exit
+    fi
+
+}
+
+main
+
