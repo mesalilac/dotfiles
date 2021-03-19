efcb5adcfcf83096c775cc41530ba71d552c7537
senpai-10 <bmjfdrh@gmail.com>
2021-03-19 06:10:20 +0300

    folders, utilities, fonts

--- scripts/arch/functions/packages.lua
+++ scripts/arch/functions/packages.lua
@@ -5,8 +5,67 @@ local menu = require "functions.menu"
 local packages = {}
 
 function packages.install()
-    print("installing packages")
+    -- setup user folders
+    os.execute([[
+        mkdir -p ~/Documents
+        mkdir -p ~/Pictures
+        mkdir -p ~/Downloads
+        mkdir -p ~/Music
+        mkdir -p ~/Videos
+        mkdir -p ~/Screenshots
+    ]])
+
+    -- utilities
+    install.pacman("zip")
+    install.pacman("unzip")
+    install.pacman("wget")
     install.pacman("nemo")
+    install.pacman("vlc")
+    install.pacman("pavucontrol")
+    install.pacman("nitrogen")
+    install.pacman("udisks2")
+    install.pacman("ntfs-3g")
+    install.pacman("neofetch")
+    install.pacman("neovim")
+    install.pacman("alsa-utils")
+    install.pacman("code")
+    install.pacman("feh")
+    install.pacman("git")
+    install.pacman("discord")
+    install.pacman("htop")
+    install.pacman("imagemagick")
+    install.pacman("nodejs")
+    install.pacman("npm")
+    install.pacman("pulseaudio")
+    install.pacman("python3")
+    install.pacman("python-pip")
+    install.pacman("rxvt-unicode")
+    install.pacman("scrot")
+    install.pacman("which")
+    install.pacman("curl")
+    install.pacman("highlight")
+    install.pacman("youtube-dl")
+    install.pacman("xbindkeys")
+
+    -- fonts
+    install.pacman("ttf-dejavu")
+    install.pacman("ttf-liberation")
+    install.pacman("ttf-font-awesome")
+    install.pacman("noto-fonts-emoji")
+    install.yay("noto-fonts-sc")
+    install.yay("ttf-ms-fonts")
+    os.execute([[
+        cd Downloads
+        wget https://support.steampowered.com/downloads/1974-YFKL-4947/SteamFonts.zip
+        unzip SteamFonts.zip -d SteamFonts/ && rm SteamFonts.zip
+        mkdir -p /usr/local/share/fonts
+        sudo mv SteamFonts/* /usr/local/share/fonts
+        rm -rf SteamFonts/
+        cd ~
+    ]])
+
+
+
 end
 
 return packages
