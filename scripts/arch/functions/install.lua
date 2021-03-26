e250c825c81690e72da0db09520ea31fd9b63c75
senpai-10 <bmjfdrh@gmail.com>
2021-03-26 13:12:54 +0300

    check if installation failed

--- scripts/arch/functions/install.lua
+++ scripts/arch/functions/install.lua
@@ -1,11 +1,11 @@
 local install = {}
 
 function install.pacman(package)
-    os.execute(string.format("sudo pacman -S %s --noconfirm", package))
+    os.execute(string.format("sudo pacman -S %s --noconfirm || echo \"- installation failed pacman %s\" >> ~/arch_script_log", package, package))
 end
 
 function install.yay(package)
-    os.execute(string.format("yay -S %s", package))
+    os.execute(string.format("yay -S %s || echo \"- installation failed yay %s\" >> ~/arch_script_log", package, package))
 end
 
 return install
