5a8ae80a41aaf92839f37188efaf65ba2e311e46
senpai-10 <bmjfdrh@gmail.com>
2021-04-14 02:02:18 +0300

    install yay

--- scripts/arch/main.sh
+++ scripts/arch/main.sh
@@ -18,6 +18,15 @@ if pacman -Qe "fzf" > /dev/null ; then
         pac "fzf"
 fi
 
+if pacman -Qe "yay" > /dev/null ; then
+        echo
+    else
+        cd ~/
+        git clone https://aur.archlinux.org/yay.git
+        cd yay
+        makepkg -si
+fi
+
 clear
 
 load "config.sh"
