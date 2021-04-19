6395866c619921ba68dd2ac1fc3d2a8f18b54368
senpai-10 <bmjfdrh@gmail.com>
2021-04-19 06:54:37 +0300

    get DIR_PATH

--- scripts/arch/config.sh
+++ scripts/arch/config.sh
@@ -4,3 +4,6 @@ video_player="mpv"
 browser="google-chrome"
 terminal="kitty"
 window_manager="dwm"
+[[ -d $HOME/projects/shell/dotfiles ]] && 
+DIR_PATH="$HOME/projects/shell/dotfiles" || 
+DIR_PATH="$HOME/dotfiles"
