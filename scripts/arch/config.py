a500f5e2b888d964d28e7abbb86b8739378a6ea2
senpai-10 <bmjfdrh@gmail.com>
2021-09-15 22:31:48 +0300

    config.py

--- scripts/arch/config.py
+++ scripts/arch/config.py
@@ -11,12 +11,16 @@ window_manager    |  dwm, openbox, bspw
 
 '''
 
+from pathlib import Path
+
+_path = str(Path().absolute())
+
 config = {
-  file_manager: "nemo",
-  music_player_cli: "cmus",
-  video_player: "mpv",
-  browser: "google-chrome",
-  terminal: "alacritty",
-  window_managerf: "bspwm",
-  path: [""]
+  "file_manager": "nemo",
+  "music_player_cli": "cmus",
+  "video_player": "mpv",
+  "browser": "google-chrome",
+  "terminal": "alacritty",
+  "window_manager": "bspwm",
+  "path": _path
 }
