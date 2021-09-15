287cfc220f63712e1d28fd8e7e55ca05a276cc69
senpai-10 <bmjfdrh@gmail.com>
2021-09-15 22:20:32 +0300

    config.py

---
+++ scripts/arch/config.py
@@ -0,0 +1,22 @@
+'''
+
+Options for config
+
+file_manager      |  nemo
+music_player_cli  |  cmus
+video_player      |  mpv
+browser           |  google-chrome, firefox
+terminal          |  st, kitty, alacritty
+window_manager    |  dwm, openbox, bspw
+
+'''
+
+config = {
+  file_manager: "nemo",
+  music_player_cli: "cmus",
+  video_player: "mpv",
+  browser: "google-chrome",
+  terminal: "alacritty",
+  window_managerf: "bspwm",
+  path: [""]
+}
