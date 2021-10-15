2768c1809ff7e8f801ec95e8f9b721160923e995
senpai-10 <bmjfdrh@gmail.com>
2021-10-15 09:11:26 +0300

    clear command

--- scripts/arch/main.py
+++ scripts/arch/main.py
@@ -1,4 +1,5 @@
 from config import config
+import os
 
 if not config["root"] or not config["home"]:
   print("Both 'root' and 'home' requirements!")
@@ -10,13 +11,17 @@ class User_input:
     self.value = ""
 
   def get(self): 
-    self.value = input("# ")
+    self.value = input("# ").lower()
 
 user_input = User_input()
 help_message = '''Arch install script
 
 commands:
 \thelp\t help message
+\texit\t exit script
+\tclear\t clear the screen
+\tsync\t sync local dotfiles with dotfiles repo
+
 '''
 
 class App:
@@ -31,8 +36,17 @@ class App:
       if user_input.value == "help":
         print(help_message)
 
-      if user_input.value == "exit":
+      elif user_input.value == "exit":
         self.exit = True
+      
+      elif user_input.value == "clear":
+        os.system("clear")
+
+      elif not user_input.value:
+        ...
+      
+      else: 
+        print(f"{user_input.value} is not a valid command")
 
 app = App()
 
