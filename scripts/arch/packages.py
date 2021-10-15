ad58bd66f8ad85de80de29200e2e658c2ccac923
senpai-10 <bmjfdrh@gmail.com>
2021-10-15 12:17:55 +0300

    install all packages

--- scripts/arch/packages.py
+++ scripts/arch/packages.py
@@ -64,3 +64,120 @@ mkdir -pv ~/AppImages
     cd dwmblocks &&
     sudo make install &&
     cd .. &&''')
+
+  pacman("zsh")
+  pacman("which")
+  pacman("zip")
+  pacman("unzip")
+  pacman("wget")
+  pacman("bashtop")
+  pacman("picom")
+  pacman("easytag")
+  pacman("ffmpegthumbnailer")
+  pacman("ffmpegthumbs")
+  pacman("pavucontrol")
+  pacman("nitrogen")
+  pacman("udisks2")
+  pacman("ntfs-3g")
+  pacman("neofetch")
+  pacman("alsa-utils")
+  pacman("code")
+  pacman("feh")
+  pacman("discord")
+  pacman("htop")
+  pacman("imagemagick")
+  pacman("nodejs")
+  pacman("npm")
+  pacman("pulseaudio")
+  pacman("python3")
+  pacman("python-pip")
+  pacman("rxvt-unicode")
+  pacman("scrot")
+  pacman("flameshot")
+  pacman("curl")
+  pacman("highlight")
+  pacman("wine")
+  pacman("youtube-dl")
+  pacman("tree")
+  pacman("xbindkeys")
+  pacman("imwheel")
+  pacman("nautilus")
+  pacman("bc")
+  pacman("lxappearance")
+  pacman("rofi")
+  pacman("gimp")
+  pacman("gnome-calculator")
+  pacman("gnome-system-monitor")
+  pacman("steam")
+  pacman("jgmenu")
+  
+  # fonts
+  pacman("ttf-dejavu")
+  pacman("ttf-liberation")
+  pacman("ttf-font-awesome")
+  pacman("noto-fonts-emoji")
+  pacman("ttf-jetbrains-mono")
+  pacman("ttf-fantasque-sans-mono")
+  pacman("noto-fonts")
+  pacman("ttf-droid")
+  pacman("terminus-font")
+
+  yay("consolas-font")
+  yay("ttf-material-icons-git")
+  yay("ttf-icomoon-feather")
+  yay("nerd-fonts-iosevka")
+  yay("noto-fonts-sc")
+  yay("ttf-ms-fonts")
+  yay("fontpreview-ueberzug-git")
+
+  yay("spotify")
+  yay("colorpicker")
+  yay("figma-linux")
+  yay("cava")
+  yay("deadd-notification-center-bin")
+  yay("spaceship-prompt-git")
+  yay("code-marketplace")
+  yay("ls-icons")
+  yay("polybar")
+
+  os.system("sudo chsh -s $(which zsh)")
+  os.system("flameshot config --showhelp false")
+  os.system("jgmenu_run init --theme=archlabs_1803")
+  os.system('sh -c "$(curl -fsSL https://starship.rs/install.sh)"')
+  os.system('''wget https://support.steampowered.com/downloads/1974-YFKL-4947/SteamFonts.zip &&
+  unzip SteamFonts.zip -d SteamFonts/ && rm SteamFonts.zip &&
+  sudo mkdir -p /usr/local/share/fonts &&
+  sudo mv SteamFonts/* /usr/local/share/fonts &&
+  rm -rf SteamFonts/''')
+  
+  os.system('''
+  git clone https://github.com/thameera/vimv.git &&
+  sudo cp vimv/vimv /usr/local/bin/ &&
+  sudo chmod +x /usr/local/bin/vimv''')
+  os.system("curl -sLf https://spacevim.org/install.sh | bash")
+
+  os.system('''
+  wget https://github.com/Superjo149/auryo/releases/download/v2.5.4/Auryo-2.5.4.AppImage &&
+  chmod +x Auryo-2.5.4.AppImage &&
+  mv Auryo-2.5.4.AppImage ~/AppImages/auryo''')
+
+  os.system('''
+  wget https://download.kde.org/stable/kdenlive/21.08/linux/kdenlive-21.08.1-x86_64.appimage &&
+  chmod +x kdenlive-20.12.3a-x86_64.appimage &&
+  mv kdenlive-20.12.3a-x86_64.appimage ~/AppImages/kdenlive''')
+
+  os.system('''
+  wget https://download.kde.org/stable/krita/4.4.8/krita-4.4.8-x86_64.appimage &&
+  chmod +x krita-4.4.8-x86_64.appimage &&
+  mv krita-4.4.8-x86_64.appimage ~/AppImages/krita''')
+
+  os.system('''
+  wget https://github.com/ppy/osu/releases/download/2021.907.0/osu.AppImage &&
+  chmod +x osu.AppImage &&
+  mv osu.AppImage ~/AppImages/osu''')
+
+  os.system('''
+  wget https://hyperbeam.com/download/linux &&
+  chmod +x linux &&
+  mv linux ~/AppImages/hyperbeam''')
+
