cf7893cca0cb885d3d4956b52af7c67e7748478c
senpai-10 <bmjfdrh@gmail.com>
2021-10-15 08:41:21 +0300

    class user_input App

--- scripts/arch/main.py
+++ scripts/arch/main.py
@@ -3,3 +3,17 @@ from config import config
 if not config["root"] or not config["home"]:
   print("Both 'root' and 'home' requirements!")
   exit(1)
+
+class User_input:
+  ''' get user input '''
+  def __init__(self):
+    self.value
+
+  def get(self): ...
+
+class App:
+  def init(self): print("hi")
+
+app = App()
+
+app.init()
