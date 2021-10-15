237ec82b22dae1449ede16056d6af44811052a91
senpai-10 <bmjfdrh@gmail.com>
2021-10-15 08:25:30 +0300

    find_root_dir and get_home_dir

--- scripts/arch/config.py
+++ scripts/arch/config.py
@@ -1,7 +1,18 @@
+import os
+
+def get_home_dir() -> str:
+  return os.path.expanduser("~")
+
 def find_root_dir() -> str: 
-  # check if ~/dotfiles exists use ~/dotfiles 
-  # else use ~/projects/shell/dotfiles 
-  ...
+  '''
+    check if dotfiles exists in ~/projects/shell/dotfiles or ~/dotfiles and return path
+  '''
+  home = get_home_dir()
+  if os.path.exists(f"{home}/projects/shell/dotfiles"):
+    return f"{home}/projects/shell/dotfiles"
+
+  if os.path.exists(f"{home}/dotfiles"):
+    return f"{home}/dotfiles"
 
 config = {
   "file_manager": "nemo",
@@ -10,5 +21,6 @@ config = {
   "browser": "brave",         # google-chrome firefox brave
   "terminal": "alacritty",    # st kitty alacritty
   "window_manager": "bspwm",   # dwm openbox bspwm,
-  "root": find_root_dir()
+  "root": find_root_dir(),
+  "home": get_home_dir()
 }
