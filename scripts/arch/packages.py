035c473d3fc3941fdfbbfc5c9d63b6f5dd881391
senpai-10 <bmjfdrh@gmail.com>
2021-10-15 11:16:29 +0300

    setup user folders

--- scripts/arch/packages.py
+++ scripts/arch/packages.py
@@ -1,11 +1,23 @@
 from tools import (pacman, yay)
 from config import config
+import os
 
 def packages():
   ''' install all packages '''
+  print("+ Setup user folders")
+  os.system('''
+mkdir -pv ~/Documents
+mkdir -pv ~/Pictures
+mkdir -pv ~/Downloads
+mkdir -pv ~/Music
+mkdir -pv ~/Videos
+mkdir -pv ~/Screenshots
+mkdir -pv ~/mpv_screenshots
+mkdir -pv ~/AppImages
+''')
 
-  pacman(config["file_manager"])
-  pacman(config["music_playercli"])
-  pacman(config["video_player"])
-  if config["browser"] == "google-chrome" or config["browser"] == "brave": yay(config["browser"])
-  if config["browser"] == "firefox": pacman(config["browser"])
+  # pacman(config["file_manager"])
+  # pacman(config["music_playercli"])
+  # pacman(config["video_player"])
+  # if config["browser"] == "google-chrome" or config["browser"] == "brave": yay(config["browser"])
+  # if config["browser"] == "firefox": pacman(config["browser"])
