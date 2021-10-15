7065730a0f878f8884a175301a4246d093db7097
senpai-10 <bmjfdrh@gmail.com>
2021-10-15 13:08:28 +0300

    copy_files.py

--- scripts/arch/copy_files.py
+++ scripts/arch/copy_files.py
@@ -6,4 +6,21 @@ def copy_files():
   root = config["root"]
   cd_root = f"cd {root}"
   
+  os.system(f'''
+  {cd_root} &&
+  mkdir -p /usr/share/cmus/ &&
+  mkdir -p ~/.icons &&
+  mkdir -p ~/.config/BetterDiscord &&
+  mkdir -p /usr/share/fonts/TTF &&
   
+  cp -rf dotfiles/.config ~/.config
+  cp -rf dotfiles/home/* ~/
+
+  git clone https://github.com/powerline/fonts.git
+  cd fonts
+  ./install.sh
+  cd ..
+  sudo cp fonts/* ~/.local/share/fonts/
+
+  sudo cp themes/cmus/*.theme /usr/share/cmus/
+  ''')
