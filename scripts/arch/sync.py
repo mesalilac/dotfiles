9ff3c5af2f799ef70dc7bd4c1ededf2742265283
senpai-10 <bmjfdrh@gmail.com>
2021-10-19 17:36:40 +0300

    sync

--- scripts/arch/sync.py
+++ scripts/arch/sync.py
@@ -9,35 +9,12 @@ def sync():
   # home
   system(f'''
     {cd_root} &&
-    cp -r home/scripts ~/ 
-    cp -r home/.themes ~/
-    cp -r home/.shell_profile ~/
-    cp -r home/.icons ~/
-    cp home/.xinitrc ~/
-    cp home/.bashrc ~/
-    cp home/.zshrc ~/
-    cp home/.zprofile ~/
-    cp home/.zshenv ~/
-    cp home/.yarnrc ~/
-    cp home/.xbindkeysrc ~/
-    cp home/.imwheelrc ~/
+    cp -r home/ ~/ 
   ''')
 
   # .config
 
   system(f'''
     {cd_root} &&
-    cp .config/bspwm -r ~/.config
-    cp .config/btops -r ~/.config
-    cp .config/compton -r ~/.config
-    cp .config/jgmenu -r ~/.config
-    cp .config/kitty -r ~/.config
-    cp .config/deadd -r ~/.config
-    cp .config/mpv -r ~/.config
-    cp .config/nvim -r ~/.config
-    cp .config/openbox -r ~/.config
-    cp .config/polybar -r ~/.config
-    cp .config/sxhkd -r ~/.config
-    cp .config/dxhd -r ~/.config
-    cp .config/picom.conf ~/.config
+    cp .config/ -r ~/.config
   ''')
