af1cee772eacdfc978905daae45cde8a99d3753a
Abdalrzag Eisa <bmjfdrh@gmail.com>
2024-12-04 20:15:58 +0300

    backup: 2024-12-04 08:15:45 PM

--- dotfiles/.config/zsh/.zprofile
+++ dotfiles/.config/zsh/.zprofile
@@ -1,5 +1,6 @@
 if [[ -z $DISPLAY && $(tty) = "/dev/tty1" ]]; then
     udiskie -aNq &
+
     exec startx
 fi
 
--- dotfiles/.local/share/nemo/actions/scripts/set_emblem.sh
+++ dotfiles/.local/share/nemo/actions/scripts/set_emblem.sh
@@ -3,10 +3,23 @@
 # gio set -t unset "${dir}" metadata::custom-icon
 
 for dir in "$@"; do
-    IMAGE="$(ls "${dir}"/*.{png,jpg,jpeg} 2>/dev/null | head -1)"
+    if [[ -d "${dir}" ]]; then
+        IMAGE="${dir}/$(ls "${dir}" | grep -E '^[0-9]+\.(png|jpe?g)$' | sort -V | head -n 1)"
+        NEW_IMAGE_PATH=""${HOME}"/.cache/custom-icons"
+        NEW_IMAGE="${NEW_IMAGE_PATH}/$(sha256sum "${IMAGE}" | cut -d ' ' -f 1)"
 
-    # echo "${IMAGE}"
+        mkdir -p "${NEW_IMAGE_PATH}"
+        cp "${IMAGE}" "${NEW_IMAGE}"
 
-    gio set "${dir}" metadata::custom-icon "file://${IMAGE}"
-    # gio set -t stringv "${dir}" metadata::emblems emblem-default
+        # normalize image filename
+        # this doesn't work
+        # IMAGE="$(printf '%s' "${IMAGE}" | jq -sRr @uri)"
+
+        # echo "${IMAGE}"
+
+        if [[ -f "${NEW_IMAGE}" ]]; then
+            gio set "${dir}" metadata::custom-icon "file://${NEW_IMAGE}"
+        fi
+        # gio set -t stringv "${dir}" metadata::emblems emblem-default
+    fi
 done
