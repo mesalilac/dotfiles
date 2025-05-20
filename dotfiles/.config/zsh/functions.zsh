5680cb9c1601c1ca2d18395b6006f3c189eb1818
Abdalrzag Eisa <bmjfdrh@gmail.com>
2025-05-20 19:22:38 +0300

    backup: 2025-05-20 07:22:26 PM

--- dotfiles/.config/zsh/functions.zsh
+++ dotfiles/.config/zsh/functions.zsh
@@ -103,19 +103,19 @@ yt-select() {
     URL=$1
     [ -z $URL ] && echo -e "You must provide a URL." && return 1
     format_id=$(yt-dlp --list-formats $URL | tail -n +6 |  fzf --layout=reverse | awk '{print $1}')
-    yt-dlp -o "%(title)s.%(ext)s" --windows-filenames --embed-thumbnail --add-metadata -ic -f $format_id $URL
+    yt-dlp -o "%(title)s.%(ext)s" --windows-filenames --add-metadata -ic -f $format_id $URL
 }
 
 yt-audio() {
     URL=$1
     [ -z $URL ] && echo -e "You must provide a URL." && return 1
-    yt-dlp -o "%(title)s.%(ext)s" --windows-filenames --audio-quality 0 --embed-thumbnail --add-metadata -ic --extract-audio -t mp3 "$URL"
+    yt-dlp -o "%(title)s.%(ext)s" --windows-filenames --add-metadata -ic --extract-audio --audio-format mp3 "$URL"
 }
 
 yt() {
     URL=$1
     [ -z $URL ] && echo -e "You must provide a URL." && return 1
-    yt-dlp -o "%(title)s.%(ext)s" --windows-filenames --embed-thumbnail --add-metadata -ic -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best" $URL
+    yt-dlp -o "%(title)s.%(ext)s" --windows-filenames --add-metadata -ic -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best" $URL
 }
 
 yt-playlist() {
@@ -124,7 +124,6 @@ yt-playlist() {
 
     yt-dlp -o "[%(playlist_index)s-%(playlist_count)s] %(title)s.%(ext)s" \
         --windows-filenames \
-        --embed-thumbnail \
         --add-metadata \
         -ic \
         -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best" \
